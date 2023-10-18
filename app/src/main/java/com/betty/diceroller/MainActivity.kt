91a9f082c1ddaea94b41daa648f98aabaa6218ff
betty2310 <huynh.dh2310@gmail.com>
2023-10-18 22:10:25 +0700

    refactor: delete unused import and use val

--- app/src/main/java/com/betty/diceroller/MainActivity.kt
+++ app/src/main/java/com/betty/diceroller/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.betty.diceroller
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -12,13 +10,13 @@ import com.betty.diceroller.databinding.ActivityMainBinding
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.btnName = "roll"
 
-        var diceImage1 = binding.diceImage
-        var diceImage2 = binding.diceImage2
+        val diceImage1 = binding.diceImage
+        val diceImage2 = binding.diceImage2
 
-        var rollButton = binding.rollButton
+        val rollButton = binding.rollButton
         rollButton.setOnClickListener {
             Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
             rollDice(diceImage1, diceImage2)
